f3ce8cf74351918b707c8174914dbe5108b4973d
louis <quocbinhgithub@gmail.com>
2023-01-28 21:17:48 +0700

    [add] get menthod

--- Program.cs
+++ Program.cs
@@ -10,17 +10,38 @@ using System.Text;
 namespace NetWorking{
     class Program
     {
+        static void ShowHeader(HttpResponseHeaders headers){
+          foreach (var header in headers)
+          {
+            Console.WriteLine($"{header.Key} - {header.Value}");
+          }
+        }
+
         public static async Task<string> GetWebContent(string url){
-            var httpClient = new HttpClient();
-            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
-            string responseHtml = await httpResponseMessage.Content.ReadAsStringAsync();
-            return responseHtml;
+            using var httpClient = new HttpClient();
+            try
+            {
+                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);               
+
+                ShowHeader(httpResponseMessage.Headers);
+                string responseHtml = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                return "adasd";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Loi";
+            }
+       
         }
         static void Main(string[] args){
             var url = "https://xuanthulab.net/networking-su-dung-httpclient-trong-c-tao-cac-truy-van-http.html";
-            Task<string> result  = GetWebContent(url);
+            Task<string> respone  =  GetWebContent(url);
+            respone.Wait();
+            var html = respone.Result;
             Console.WriteLine("Get respone ");
-            Console.WriteLine(result);
+            Console.WriteLine(html);
    
         }
     }
