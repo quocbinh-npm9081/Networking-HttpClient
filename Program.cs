bf5c7e7cb0f258582bcf4bc6672868b5a2c34098
louis <quocbinhgithub@gmail.com>
2023-01-28 20:21:56 +0700

    [add] add get menthods

--- Program.cs
+++ Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 namespace NetWorking{
     class Program
     {
+        public static async Task<string> GetWebContent(string url){
+            var httpClient = new HttpClient();
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+            string responseHtml = await httpResponseMessage.Content.ReadAsStringAsync();
+            return responseHtml;
+        }
         static void Main(string[] args){
-            string url = "https://xuanthulab.net/lap-trinh/csharp/?page=3#acff";
-            var uri = new Uri(url);
-            Console.WriteLine(uri.Port);
-            var uriType = uri.GetType();
-            // uriType.GetProperties().ToList().ForEach(property => {
-            //     Console.WriteLine($"{property.Name} - {property.GetValue(uri)}");
-            // });
-            Console.WriteLine(uri.Port);
-            
-
-            //Host name
-            var hostName = Dns.GetHostName();
-            Console.WriteLine(hostName); //DESKTOP-JNTN2DM host hien tai
-
-            //ping remote
-            var ping = new Ping();
-            var pingReply = ping.Send("google.com.vn"); /// ping den dns google
-            Console.WriteLine("+++ PING MAY GOOGLE DNS");
-            Console.WriteLine(pingReply.Status);
-            if (pingReply.Status == IPStatus.Success)
-            {
-                Console.WriteLine(pingReply.RoundtripTime);
-                Console.WriteLine(pingReply.Address);
-            }
+            var url = "https://xuanthulab.net/networking-su-dung-httpclient-trong-c-tao-cac-truy-van-http.html";
+            Task<string> result  = GetWebContent(url);
+            Console.WriteLine("Get respone ");
+            Console.WriteLine(result);
    
         }
     }
