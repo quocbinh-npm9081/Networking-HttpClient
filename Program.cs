dba26fa17e708435efadce0457133b44e0ef31ba
louis <quocbinhgithub@gmail.com>
2023-01-28 20:12:31 +0700

    [add] add dns ping host

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿namespace NetWorking{
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+namespace NetWorking{
     class Program
     {
         static void Main(string[] args){
-            Console.WriteLine("OK");
+            string url = "https://xuanthulab.net/lap-trinh/csharp/?page=3#acff";
+            var uri = new Uri(url);
+            Console.WriteLine(uri.Port);
+            var uriType = uri.GetType();
+            // uriType.GetProperties().ToList().ForEach(property => {
+            //     Console.WriteLine($"{property.Name} - {property.GetValue(uri)}");
+            // });
+            Console.WriteLine(uri.Port);
+            
+
+            //Host name
+            var hostName = Dns.GetHostName();
+            Console.WriteLine(hostName); //DESKTOP-JNTN2DM host hien tai
+
+            //ping remote
+            var ping = new Ping();
+            var pingReply = ping.Send("google.com.vn"); /// ping den dns google
+            Console.WriteLine("+++ PING MAY GOOGLE DNS");
+            Console.WriteLine(pingReply.Status);
+            if (pingReply.Status == IPStatus.Success)
+            {
+                Console.WriteLine(pingReply.RoundtripTime);
+                Console.WriteLine(pingReply.Address);
+            }
+   
         }
     }
 }
